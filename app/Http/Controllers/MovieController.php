91e7f5e1f02fa9b3a3985ab1b9fb177fc84fd903
Simone823 <simone.daglio823@outlook.it>
2022-04-21 16:07:40 +0200

    movie controller function index

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index () {
 
+        $movies = Movie::all();
 
-        return view('home');
+        return view('home', compact('movies'));
     }
 }
