630fab7479cf92fca17b1c89e02293282e5b0606
Simone823 <simone.daglio823@outlook.it>
2022-04-21 16:07:54 +0200

    component home, foreach movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,21 @@
 </head>
 <body>
 
-    
+    {{-- Movies wrapper --}}
+    <div class="movies_wrapper">
+
+        @foreach ($movies as $element)
+            {{-- Movie --}}
+            <div class="movie">
+                <h1 class="title">{{$element['title']}}</h1>
+                <h3 class="original_title">{{$element['original_title']}}</h3>
+                <p class="nationality">{{$element['nationality']}}</p>
+                <p class="date">{{$element['date']}}</p>
+                <p class="vote">{{$element['vote']}}</p>
+            </div>
+        @endforeach
+
+    </div>
     
 </body>
 </html>
